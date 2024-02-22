c1f8f009534376615fb1b9060cdbd86c4d810a39
axelrios <ax.rios@hotmail.com>
2024-02-22 01:12:19 -0300

    calculador de cuotas terminado

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 //Calculador de las cuotas de un producto 
 //Una vez que se presiona el boton se inicia la compra, se introduce el precio del producto, si el precio supera 
-//los 50000 se le hace un 10% de descuento y si no lo hace se calcula el precio de las cuotas sin decuento 
+//los 50000 se le hace un 10% de descuento y si no lo hace se calcula el precio de las cuotas sin descuento 
 //ACLARACION: utilize la funcion para poder adjuntarlo al boton en el index y que cuando se presione suceda este proceso
 //entiendo que no esta bien utilizada 
 
